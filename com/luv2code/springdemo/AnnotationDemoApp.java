2019f401a0f3bc86e1b078c8e87e8dcc596f7abb
Ray Guo <siruiguo@outlook.com>
2021-05-11 00:25:41 +0800

    autowiring constructor injection

--- com/luv2code/springdemo/AnnotationDemoApp.java
+++ com/luv2code/springdemo/AnnotationDemoApp.java
@@ -9,6 +9,7 @@ public class AnnotationDemoApp {
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		Coach theCoach = context.getBean("tennisCoach", Coach.class);
 		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
 		context.close();
 	}
 
