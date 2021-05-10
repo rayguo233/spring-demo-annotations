15b573896e82521adac926f2f70c04d0c9864002
Ray Guo <siruiguo@outlook.com>
2021-05-11 00:57:19 +0800

    qualifiers for dependency injection

---
+++ com/luv2code/springdemo/RandomFortuneService.java
@@ -0,0 +1,23 @@
+package com.luv2code.springdemo;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+
+	private String[] data = {
+			"fortune 1", "fortune 2", "fortune 3"
+	};
+	
+	private Random myRandom = new Random();
+	
+	@Override
+	public String getFortune() {
+		int index = myRandom.nextInt(data.length);
+		String theFortune = data[index];
+		return theFortune;
+	}
+
+}
