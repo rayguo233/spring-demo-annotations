a4550c9ff653d92861badd4a397957e1cbe7ba8e
Ray Guo <siruiguo@outlook.com>
2021-05-11 02:39:05 +0800

    life cycle annotation

--- com/luv2code/springdemo/TennisCoach.java
+++ com/luv2code/springdemo/TennisCoach.java
@@ -1,5 +1,8 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -13,6 +16,16 @@ public class TennisCoach implements Coach {
 	
 	public TennisCoach() {
 		System.out.println("TennisCoach: inside default constructor.");
+	}
+	
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside doMyStartupStuff()");
+	}
+	
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside doMyCleanupStuff()");
 	}
 	
 //	@Autowired
